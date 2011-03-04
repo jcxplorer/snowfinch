de4bb845b1e776cd08ba293429883ad21ea45557
Joao Carlos <mail@joao-carlos.com>
2011-03-04 12:59:09 +0200

    Refactoring for refactoring's sake.

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -4,7 +4,5 @@ Autotest.add_discovery { "rails" }
 Autotest.add_discovery { "rspec2" }
 
 Autotest.add_hook :initialize do |at|
-  at.add_mapping(/^spec\/acceptance\/.*_spec.rb$/) do |filename|
-    filename
-  end
+  at.add_mapping(/^spec\/acceptance\/.*_spec.rb$/) { |f| f }
 end
