25754599dca6b9955fd80c057013353219a1ebf4
Joao Carlos <mail@joao-carlos.com>
2011-03-10 23:53:38 +0200

    Improves charting performance and should fix the memory leak.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,43 +7,45 @@ function updateSiteCounters() {
     activeVisitors.text(data.active_visitors);
     pageviewsToday.text(data.pageviews_today);
     visitorsToday.text(data.visitors_today);
+
+    setTimeout(updateSiteCounters, 1000);
   });
 }
 
-function plotChart() {
+function createChart() {
+  var options = {
+        lines: { show: true, fill: true },
+        points: { show: true },
+        colors: ["#ccc", "#d18b2c"],
+        yaxis: { min: 0, tickDecimals: 0 },
+        xaxis: { tickSize: 2, tickFormatter: function(number) {
+          if (number == 0) { return ""; }
+          else if (number < 10) { return "0" + number + ":00"; }
+          else { return number + ":00"; }
+        } }
+      },
+      data = [],
+      chart = $.plot("#chart", data, options);
+
+  updateChart(chart);
+}
+
+function updateChart(chart) {
+  console.log(chart);
   $.getJSON($("#chart").data("source"), function(data) {
-    $.plot("#chart", [
-           { data: data.yesterday },
-           { data: data.today }
-    ],
-    {
-      lines: { show: true, fill: true },
-      points: { show: true },
-      colors: ["#ccc", "#d18b2c"],
-      yaxis: { min: 0, tickDecimals: 0 },
-      xaxis: { tickSize: 2, tickFormatter: function(number) {
-        if (number == 0) {
-          return "";
-        }
-        else if (number < 10) {
-          return "0" + number + ":00";
-        }
-        else {
-          return number + ":00";
-        };
-      } }
-    });
+    chart.setData([data.yesterday, data.today]);
+    chart.setupGrid();
+    chart.draw();
+    setTimeout(updateChart, 1000, chart);
   });
 }
 
 jQuery(function() {
   if ($("#counters[data-source]").length) {
     updateSiteCounters();
-    setInterval(updateSiteCounters, 1000);
   }
 
   if ($("#chart[data-source]").length) {
-    plotChart();
-    setInterval(plotChart, 1000);
+    createChart();
   }
 });
