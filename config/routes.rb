34f1b968c8c70dc5251fbdb69252137ca8f0b2b9
Joao Carlos <mail@joao-carlos.com>
2011-05-15 22:50:06 +0300

    Snowfinch::Collector mounting was disabled by mistake.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Snowfinch::Application.routes.draw do
 
   devise_for :users
 
-  #match "/collector" => Snowfinch::Collector
+  match "/collector" => Snowfinch::Collector
 
   resources :sites do
     member do
